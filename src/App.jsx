e6a9af43cca3043f045f0ca2e11eff34daead1e6
luis <luisaravena.71@gmail.com>
2025-06-30 17:31:25 -0400

    certamen

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import PokemonFetcher from './PokemonFetcher'
+
+function App() {
+
+  return (
+    <>
+      <h1>Conoce a tus Pokémon</h1>
+      <PokemonFetcher /> 
+    </>
+  )
+}
+
+export default App
